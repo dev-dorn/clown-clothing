acb7531bceef5c6bc18c263e914fcd1c6812aca8
dev-dorn <berrymundia@gmail.com>
2024-12-09 16:53:42 +0300

    styled the web to look like the final product

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 
-import HomePage from "./homepage.component.jsx";
+import HomePage from "./pages/homepage/homepage.component.jsx";
 function App() {
     return <div>
         <HomePage/>
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Directory from "../../components/directory.component.jsx/directory.component.jsx";
+import './hompage.styles.scss'
+const HomePage = () => (
+    <div className='home-page'>
+        <Directory/>
+    </div>
+)
+export default HomePage;
